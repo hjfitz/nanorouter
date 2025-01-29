472cd8cc87a55503e1866d0421f1344303147225
Harry Fitzgerald <harry.fitzgerald@globalfreightsolutions.com>
2025-01-29 16:50:57 +0000

    lint and format

--- index.js
+++ index.js
@@ -1,42 +1,33 @@
-const routes = []
+const routes = [];
 
-export const router = new Proxy({}, {
-  get: (_, method) => (path, handler) => {
-	method = ['any', 'use'].includes(method) ? '*' : method.toUpperCase()
-	routes.unshift({
-      path, 
-      method, 
-      handler,
-    })
-  }
-})
+export const router = new Proxy(
+	{},
+	{
+		get: (_, method) => (path, handler) => {
+			method = ["any", "use"].includes(method) ? "*" : method.toUpperCase();
+			routes.unshift({
+				path,
+				method,
+				handler,
+			});
+		},
+	},
+);
 
-/**
- * @param {Request} req
- * @param {Response} res
- */
 export function route(req, res) {
-  const valid = new Set(['*', req.method, req.url])
+	const valid = new Set(["*", req.method, req.url]);
 
-  /**
-   * @param {number} index
-   */
-  const handle = (idx) => {
-    if (idx < 0) {
-	  res.writeHead(404).end('')
-      return
-	}
+	const handle = (idx) => {
+		if (idx < 0) return;
 
-    const route = routes[idx]
+		const route = routes[idx];
 
-    const next = () => handle(idx - 1)
+		const next = () => handle(idx - 1);
 
-    const matches = valid.has(route.method) && valid.has(route.path)
+		const matches = valid.has(route.method) && valid.has(route.path);
 
-    matches 
-      ? route.handler(req, res, next)
-      : next()
-  }
+		matches ? route.handler(req, res, next) : next();
+	};
 
-  handle(routes.length - 1)
+	handle(routes.length - 1);
 }
