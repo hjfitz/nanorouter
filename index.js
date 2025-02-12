acd9798511d276f86d5bc587080594d3131c9995
Harry Fitzgerald <harry.fitzgerald@globalfreightsolutions.com>
2025-02-12 16:24:37 +0000

    chore: cleanup

--- index.js
+++ index.js
@@ -1,33 +1,25 @@
-const routes = [];
+const routes = []
 
-export const router = new Proxy(
-	{},
-	{
+export const router = new Proxy({}, {
 		get: (_, method) => (path, handler) => {
-			method = ["any", "use"].includes(method) ? "*" : method.toUpperCase();
-			routes.unshift({
-				path,
-				method,
-				handler,
-			});
+			method = ["any", "use"].includes(method) ? "*" : method.toUpperCase()
+			routes.unshift({ path, method, handler })
 		},
 	},
-);
+)
 
 export function route(req, res) {
-	const valid = new Set(["*", req.method, req.url]);
+	const valid = new Set(["*", req.method, req.url])
 
 	const handle = (idx) => {
-		if (idx < 0) return;
+		if (idx < 0) return
 
-		const route = routes[idx];
+		const route = routes[idx]
+		const matches = valid.has(route.method) && valid.has(route.path)
 
-		const next = () => handle(idx - 1);
+		const next = () => handle(idx - 1)
+		matches ? route.handler(req, res, next) : next()
+	}
 
-		const matches = valid.has(route.method) && valid.has(route.path);
-
-		matches ? route.handler(req, res, next) : next();
-	};
-
-	handle(routes.length - 1);
+	handle(routes.length - 1)
 }
--- sample.js
+++ sample.js
@@ -1,37 +1,34 @@
-import http from 'node:http'
-import { router, route } from './index.js'
+import http from "node:http";
+import { route, router } from "./index.js";
 
-router.get('/hello', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Hello, GET!')
-})
+router.get("/hello", (_, res) => {
+	res.writeHead(200, { "Content-Type": "text/plain" });
+	res.end("Hello, GET!");
+});
 
-router.post('/world', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Hello, POST!')
-})
+router.post("/world", (_, res) => {
+	res.writeHead(200, { "Content-Type": "text/plain" });
+	res.end("Hello, POST!");
+});
 
-router.any('/any', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end(`Hello, ${req.method}!`)
-})
-
+router.any("/any", (_, res) => {
+	res.writeHead(200, { "Content-Type": "text/plain" });
+	res.end(`Hello, ${req.method}!`);
+});
 
 //Example with next function
-router.any('*', (req, res, next) => {
-    console.log("First handler")
-    next()
-})
+router.any("*", (_, __, next) => {
+	console.log("First handler");
+	next();
+});
 
-router.get('/chained', (req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.end('Chained Handler')
-})
+router.get("/chained", (_, res) => {
+	res.writeHead(200, { "Content-Type": "text/plain" });
+	res.end("Chained Handler");
+});
 
-const server = http.createServer(route)
+const server = http.createServer(route);
 
-await new Promise(res => server.listen(3001, res))
+await new Promise((res) => server.listen(3001, res));
 
-console.log('Server listening on port 3000')
-
-
+console.log("Server listening on port 3000");
